8a8eff319345dd88c2f670deea235209f49abac1
nicoleweese <nweese@ymail.com>
2018-01-24 15:33:26 -0800

    commented out stuff

--- server.js
+++ server.js
@@ -15,19 +15,15 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/test', (req, res) => res.send('hello world'));
 
-app.get('/api/v1/books/:id', (request, response, next) => {
+app.get('/api/v1/books/:id', (request, response) => {
   console.log(request.params.id);
-  next();
-}, function(req, res, next) {
-  
   client.query(`
-  SELECT * FROM books WHERE books_id = ${res.id};
+  SELECT * FROM books WHERE books_id = ${request.params.id};
 `)
-    .then(result => res.send(result.rows))
+    .then(result => response.send(result.rows))
     .catch(console.error);
-
 });
-  
+
 
 app.get('/api/v1/books', function (request, response) {
   client.query('SELECT books_id, title, author, image_url FROM books;')
@@ -70,34 +66,34 @@ app.listen(PORT, () => {
 });
 
 
-
+// THIS WAS USED ONCE TO LOAD FILL DATABASE WITH THE BOOKS TABLE, THIS ONLY NEEDS TO OCCUR ONCE OR IF THE DATABASE IS DELETED SO WE COMMENTED IT OUT FOR FUTURE USAGE.
 //////// ** DATABASE LOADERS ** ////////
 ////////////////////////////////////////
-function loadBooks() {
-  fs.readFile('../book-list-client/data/books.json', (err, fd) => {
-    JSON.parse(fd.toString()).forEach(ele => {
-      client.query(
-        'INSERT INTO books(author, title, isbn, image_url, description) VALUES($1, $2, $3, $4, $5) ON CONFLICT DO NOTHING',
-        [ele.author, ele.title, ele.isbn, ele.image_url, ele.descriotion]
-      )
-        .catch(console.error);
-    })
-  })
-}
+// function loadBooks() {
+//   fs.readFile('../book-list-client/data/books.json', (err, fd) => {
+//     JSON.parse(fd.toString()).forEach(ele => {
+//       client.query(
+//         'INSERT INTO books(author, title, isbn, image_url, description) VALUES($1, $2, $3, $4, $5) ON CONFLICT DO NOTHING',
+//         [ele.author, ele.title, ele.isbn, ele.image_url, ele.descriotion]
+//       )
+//         .catch(console.error);
+//     })
+//   })
+// }
 
-function createTable() {
-  client.query(`
-    CREATE TABLE IF NOT EXISTS books(
-      books_id SERIAL PRIMARY KEY,
-      author VARCHAR(60),
-      title VARCHAR(80),
-      isbn VARCHAR(23),
-      image_url VARCHAR(255),
-      description VARCHAR(500)
-    );`
-  )
-    .then(function (response) {
-      console.log('created books table in db!!!!');
-    })
-    .then(loadBooks)
-}
+// function createTable() {
+//   client.query(`
+//     CREATE TABLE IF NOT EXISTS books(
+//       books_id SERIAL PRIMARY KEY,
+//       author VARCHAR(60),
+//       title VARCHAR(80),
+//       isbn VARCHAR(23),
+//       image_url VARCHAR(255),
+//       description VARCHAR(500)
+//     );`
+//   )
+//     .then(function (response) {
+//       console.log('created books table in db!!!!');
+//     })
+//     .then(loadBooks)
+// }
